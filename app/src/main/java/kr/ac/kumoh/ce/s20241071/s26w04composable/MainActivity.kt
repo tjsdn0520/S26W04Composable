c222b425e783049ad9f5257da9a9f30eacc5d1e1
tjsdn0520 <tjsdn0530@gmail.com>
2026-09-23 14:26:40 +0900

    🚧카운터 Composable

--- app/src/main/java/kr/ac/kumoh/ce/s20241071/s26w04composable/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20241071/s26w04composable/MainActivity.kt
@@ -1,17 +1,34 @@
 package kr.ac.kumoh.ce.s20241071.s26w04composable
 
+import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontVariation.weight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.ce.s20241071.s26w04composable.ui.theme.S26W04ComposableTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +41,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
 }
 
 @Composable
@@ -33,8 +49,43 @@ fun MainScreen() {
         Column(
             modifier = Modifier.padding(innerPadding),
         ) {
-
+            Counter()
         }
     }
 }
 
+
+@Composable
+fun ColumnScope.Counter(){
+    var count by remember { mutableIntStateOf(0) }
+
+    Column(
+        modifier = Modifier
+            .weight(1F)
+            .padding(8.dp)
+            .background(Color(0XFFE9F680)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+        Text(
+            text = count.toString(),
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+                .background(Color(0xFFFE7A36)),
+            color = Color.White,
+            fontSize = 100.sp,
+            textAlign = TextAlign.Center,
+        )
+
+        Button(
+            modifier = Modifier.padding(8.dp).fillMaxWidth(),
+            onClick = {
+                count++
+            }
+        ) {
+            Text("증가", fontSize = 30.sp)
+        }
+    }
+}
